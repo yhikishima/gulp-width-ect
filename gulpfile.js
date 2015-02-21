2cfcc1528986c042729bc4d630811c1f207d2e2f
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-02-21 14:55:24 +0900

    change icnfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,12 @@
 // パッケージをインポートする
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*'],
+  replaceString: /\bgulp[\-.]/
+});
 var fs = require('fs');
-var plugin = require('gulp-load-plugins')();
-
-var compass = require('gulp-compass');
-var connect = require('gulp-connect');
-var watch = require('gulp-watch');
-var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var runSequence = require('run-sequence');
-var clean = require('gulp-clean');
-var ect = require('gulp-ect');
-var plumber = require('gulp-plumber');
-var spritesmith = require("gulp.spritesmith");
-var typescript = require('gulp-tsc');
 var del = require('del');
 var browserify = require ('browserify');
 var karma = require('karma').server;
@@ -27,9 +20,12 @@ var pass = {
   data: dir.src + '/data'
 };
 
+//---------------------
+// タスクの定義
+//---------------------
 // 簡易サーバー
 gulp.task('connect', function() {
-  return connect.server({
+  return $.connect.server({
     port: 3000, // ポート番号を設定
     root: dir.dist, // ルートディレクトリの場所を指定
     livereload: true // ライブリロードを有効にする
@@ -41,7 +37,7 @@ gulp.task("reload", function() {
   gulp.src([
     dir.dist + '/{,**/}*'
     ])
-  .pipe(connect.reload());
+  .pipe($.connect.reload());
 });
 
 // ファイルのコピー
@@ -56,7 +52,7 @@ gulp.task('copy', function () {
 
 // 不要なファイルを削除する
 // distフォルダ内を一度全て削除する
-gulp.task('clean-dist', function (cb) {
+gulp.task('clean', function (cb) {
   del([
     dir.dist + '/**'
   ], cb);
@@ -66,16 +62,41 @@ gulp.task('clean-dist', function (cb) {
 // 画像を圧縮
 gulp.task('imagemin', function () {
   gulp.src( dir.src + '/{,**/}*.{png,jpg,gif}' ) // 読み込みファイル
-    .pipe(imagemin())
+    .pipe($.imagemin())
     .pipe(gulp.dest( dir.dist )); // 書き出し先
+});
+
+// svg を圧縮する
+gulp.task('svg-min', function() {
+  gulp.src(dir.src + '/img/svg/*.svg')
+    .pipe($.svgmin())
+    .pipe(dir.src + '/img/svg/*.svg');
+});
+
+// アイコンフォント生成
+gulp.task('fonts', ['svg-min'], function() {
+  gulp.src(dir.src + '/img/fonts')
+    .pipe($.clean());
+  gulp.src(dir.src + '/img/svg/*.svg')
+    .pipe($.plumber())
+    .pipe($.iconfontCss({
+      fontName:   'icon-font',
+      path:       dir.src + '/scss/functions/_icons.scss', // _icon.scss のテンプレート
+      targetPath: dir.src + '/scss/_icons.scss', // _icon.scss を生成
+      fontPath:   dir.src + "/fonts/"})) // _icon.scss で使うフォントのパス
+    .pipe($.iconfont({
+      fontName: 'icon-font',
+      appendCodepoints: false
+    }))
+    .pipe(gulp.dest( dir.src + "/fonts/" ));
 });
 
 //Compassのタスク
 gulp.task('compass',function(){
 gulp.src( [dir.src + '/{,**/}*.scss'] )
-  .pipe(plumber())
-  .pipe(imagemin())
-  .pipe(compass({ //コンパイルする処理
+  .pipe($.plumber())
+  .pipe($.imagemin())
+  .pipe($.compass({ //コンパイルする処理
     config_file : 'config.rb',
     comments : false,
     css : dir.dist + '/css/',
@@ -86,8 +107,8 @@ gulp.src( [dir.src + '/{,**/}*.scss'] )
 //sprite作成
 gulp.task('sprite', function() {
   var spriteData = gulp.src(dir.src + '/img/sprites/*.png')
-  .pipe(plumber())
-  .pipe(spritesmith({
+  .pipe($.plumber())
+  .pipe($.spritesmith({
     imgName: 'sprite.png',
     cssName: '_sprite.scss',
     imgPath: '../img/sprite.png',
@@ -102,10 +123,10 @@ gulp.task('sprite', function() {
 });
 
 // typescript
-gulp.task('typescript', function(){
+gulp.task('tsc', function(){
   gulp.src([dir.src + '/ts/{,**/}*.ts'])
-    .pipe(plumber())
-    .pipe(typescript({
+    .pipe($.plumber())
+    .pipe($.tsc({
       removeComments: true
     }))
     .pipe(gulp.dest(dir.dist + '/js/'));
@@ -116,7 +137,7 @@ gulp.task('ect', function(){
   var json = JSON.parse(fs.readFileSync(pass.data + '/data.json'));
 
   gulp.src(dir.src + '/templates/*.ect')
-  .pipe(ect({
+  .pipe($.ect({
     data: function (file, cb) {
       cb({
         filename: file,
@@ -148,25 +169,27 @@ gulp.task('watch', function() {
 
   gulp.watch([
     dir.src + '/{,**/}*.ts' // 対象ファイル
-  ],['typescript']); // 実行タスク（css 開発用）
+  ],['tsc']); // 実行タスク（css 開発用）
 
   gulp.watch([
       dir.dist + '/{,**/}*'
   ], ["reload"]);
 });
 
-// タスクの登録
+//---------------------
+// タスクの実行
+//---------------------
 // 開発用
 gulp.task('serve', [
   'connect',
   'ect',
   'sprite',
   'compass',
-  'typescript',
+  'tsc',
   'copy',
   'watch'
 ]);
 
 gulp.task('clean', [
-  'clean-dist'
+  'clean'
 ]);
